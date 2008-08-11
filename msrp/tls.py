2c51ecf9451a41543ad16bd3ceed85afaf5b4b3c
Ruud Klaver <ruud@ag-projects.com>
2008-08-11 13:41:43 +0000

    Removed no longer present CertificateList object from tls.py imports

--- msrp/tls.py
+++ msrp/tls.py
@@ -14,7 +14,7 @@
 #    You should have received a copy of the GNU General Public License along
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-__all__ = ['Certificate', 'CertificateList', 'PrivateKey']
+__all__ = ['Certificate', 'PrivateKey']
 
 from gnutls.crypto import X509Certificate,  X509PrivateKey
 
