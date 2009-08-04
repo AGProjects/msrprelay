8bac0810ca11c553853182e60e0e8c47f38382ab
Adrian Georgescu <ag@ag-projects.com>
2009-08-04 07:53:31 +0000

    Fixed debian dependencies and increased version

--- msrp/__init__.py
+++ msrp/__init__.py
@@ -15,7 +15,7 @@
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 runtime_directory = "/var/run/msrprelay"
 system_config_directory = "/etc/msrprelay"
