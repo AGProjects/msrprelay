22f7ff38152f8ce8ea72f9f12439667e2eefeeee
Saul Ibarra <saul@ag-projects.com>
2012-06-06 11:59:05 +0000

    Added SIPThor integration

--- msrp/backend/sipthor.py
+++ msrp/backend/sipthor.py
@@ -15,17 +15,28 @@
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+import cjson
+import signal
+
+from application import log
+from application.configuration import ConfigSection, ConfigSetting
+from application.python.types import Singleton
+from application.system import host
+from application.process import process
+from gnutls.interfaces.twisted import X509Credentials
+from gnutls.constants import COMP_DEFLATE, COMP_LZO, COMP_NULL
 from sqlobject import sqlhub, connectionForURI, SQLObject, StringCol, BLOBCol
 from sqlobject.dberrors import Error as SQLObjectError
-
-from application.configuration import *
-
 from twisted.internet.threads import deferToThread
 
+from msrp import configuration_filename, __version__
 from msrp.digest import LoginFailed
-from msrp import configuration_filename
+from msrp.tls import Certificate, PrivateKey
 
-import cjson
+from thor.eventservice import EventServiceClient, ThorEvent
+from thor.entities import ThorEntitiesRoleMap, GenericThorEntity as ThorEntity
+from thor.tls import X509NameValidator
+
 
 class Config(ConfigSection):
     __cfgfile__ = configuration_filename
@@ -37,6 +48,17 @@ class Config(ConfigSection):
     username_col = "username"
     domain_col = "domain"
     profile_col = "profile"
+    multiply = 1000
+    certificate = ConfigSetting(type=Certificate, value=None)
+    private_key = ConfigSetting(type=PrivateKey, value=None)
+    ca = ConfigSetting(type=Certificate, value=None)
+
+class ThorNetworkConfig(ConfigSection):
+    __cfgfile__ = configuration_filename
+    __section__ = 'ThorNetwork'
+
+    domain = "sipthor-domain"
+    passport = X509NameValidator('O:undefined, OU:undefined')
 
 
 class Subscribers(SQLObject):
@@ -48,10 +70,56 @@ class Subscribers(SQLObject):
 
 sqlhub.processConnection = connectionForURI(Config.uri)
 
-class Checker(object):
+
+class ThorNetworkService(EventServiceClient):
+    __metaclass__ = Singleton
+    topics = ["Thor.Members"]
 
     def __init__(self):
+        self.node = ThorEntity(host.default_ip, ['msrprelay_server'], version=__version__)
+        self.networks = {}
+        self.presence_message = ThorEvent('Thor.Presence', self.node.id)
+        self.shutdown_message = ThorEvent('Thor.Leave', self.node.id)
+        credentials = X509Credentials(Config.certificate, Config.private_key, [Config.ca])
+        credentials.verify_peer = True
+        credentials.session_params.compressions = (COMP_LZO, COMP_DEFLATE, COMP_NULL)
+        EventServiceClient.__init__(self, ThorNetworkConfig.domain, credentials)
+        process.signals.add_handler(signal.SIGHUP, self._handle_SIGHUP)
+        process.signals.add_handler(signal.SIGINT, self._handle_SIGINT)
+        process.signals.add_handler(signal.SIGTERM, self._handle_SIGTERM)
+
+    def handle_event(self, event):
+        # print "Received event: %s" % event
+        networks = self.networks
+        role_map = ThorEntitiesRoleMap(event.message) ## mapping between role names and lists of nodes with that role
+        for role in ["msrprelay_server"]:
+            try:
+                network = networks[role] ## avoid setdefault here because it always evaluates the 2nd argument
+            except KeyError:
+                from thor import network as thor_network
+                network = thor_network.new(Config.multiply)
+                networks[role] = network
+            new_nodes = set([node.ip for node in role_map.get(role, [])])
+            old_nodes = set(network.nodes)
+            added_nodes = new_nodes - old_nodes
+            removed_nodes = old_nodes - new_nodes
+            if removed_nodes:
+                for node in removed_nodes:
+                    network.remove_node(node)
+                plural = len(removed_nodes) != 1 and 's' or ''
+                log.msg("removed %s node%s: %s" % (role, plural, ', '.join(removed_nodes)))
+            if added_nodes:
+                for node in added_nodes:
+                    network.add_node(node)
+                plural = len(added_nodes) != 1 and 's' or ''
+                log.msg("added %s node%s: %s" % (role, plural, ', '.join(added_nodes)))
+            #print "Thor %s nodes: %s" % (role, str(network.nodes))
+
+
+class Checker(object):
+    def __init__(self):
         self.cleartext_passwords = Config.cleartext_passwords
+        self._thor_service = ThorNetworkService()
 
     def _retrieve(self, col, username, domain):
         try:
