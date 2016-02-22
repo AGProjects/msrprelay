8acd8bbd776236598a7562065b9360baba3a8892
Saul Ibarra <saul@ag-projects.com>
2016-02-22 16:47:32 +0000

    Style
    - s/typ/cls/
    - remoded unreachable code
    - raise exceptions like we do in this century

--- msrp/tls.py
+++ msrp/tls.py
@@ -7,6 +7,7 @@ from application.process import process
 
 class _FileError(Exception): pass
 
+
 def file_content(file):
     path = process.config_file(file)
     if path is None:
@@ -20,28 +21,28 @@ def file_content(file):
     finally:
         f.close()
 
+
 class Certificate(object):
     """Configuration data type. Used to create a gnutls.crypto.X509Certificate object
        from a file given in the configuration file."""
-    def __new__(typ, value):
+    def __new__(cls, value):
         if isinstance(value, str):
             try:
                 return X509Certificate(file_content(value))
             except Exception, e:
                 raise ValueError("Certificate file '%s' could not be loaded: %s" % (value, str(e)))
-                return None
         else:
-            raise TypeError, 'value should be a string'
+            raise TypeError('value should be a string')
+
 
 class PrivateKey(object):
     """Configuration data type. Used to create a gnutls.crypto.X509PrivateKey object
        from a file given in the configuration file."""
-    def __new__(typ, value):
+    def __new__(cls, value):
         if isinstance(value, str):
             try:
                 return X509PrivateKey(file_content(value))
             except Exception, e:
                 raise ValueError("Private key file '%s' could not be loaded: %s" % (value, str(e)))
-                return None
         else:
-            raise TypeError, 'value should be a string'
+            raise TypeError('value should be a string')
